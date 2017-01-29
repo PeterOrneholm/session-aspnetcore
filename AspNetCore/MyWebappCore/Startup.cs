0c4e27fbab2b23095cece63594856197dfa1d3ff
Peter Örneholm <peter@orneholm.com>
2017-01-29 16:18:45 +0100

    Remove some initialzation code

--- AspNetCore/MyWebappCore/Startup.cs
+++ AspNetCore/MyWebappCore/Startup.cs
@@ -27,15 +27,6 @@ namespace MyWebappCore
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
             app.UseMvcWithDefaultRoute();
         }
     }
